0c1ac35d0357b0090724cc7083c6949c965264e6
Dmytro Trotsenko <trohae7@gmail.com>
2023-12-24 15:26:57 +0200

    feat: created spring app with dependencies

---
+++ src/main/java/com/expandapis/TestTaskExpandApisApplication.java
@@ -0,0 +1,13 @@
+package com.expandapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskExpandApisApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TestTaskExpandApisApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/expandapis/TestTaskExpandApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expandapis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskExpandApisApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
